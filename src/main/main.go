e320fe50e04076384948eacb560c7a28cefb54f4
lneukirchner <lneukirchner123@gmail.com>
2021-04-07 14:35:51 -0400

    separated 2d linear regression into its own func.

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+func determinant(matrix [][]float64) float64 {
+	var determinant float64
+	if len(matrix) > 2 {
+
+	} else {
+
+	}
+}
+
 func matrixTransform2d(matrix [2][2]float64, vector [2]float64) [2]float64 {
 	var result [2]float64                                               //define result vector as a list with two items
 	result[0] = (vector[0] * matrix[0][0]) + (vector[1] * matrix[1][0]) //x component
@@ -34,8 +43,29 @@ func inverseMatrix2d(matrix [2][2]float64) [2][2]float64 {
 	return newMatrix
 }
 
+func linearRegression2d(pointsList [][2]float64) [2]float64 {
+	var xSquaredTotal, xTotal, xyTotal, yTotal, oneTotal float64
+	var j int
+	var xValues, yValues []float64
+	for j = 0; j < len(pointsList); j++ {
+		xSquaredTotal += pointsList[j][0] * pointsList[j][0]
+		xTotal += pointsList[j][0]
+		xyTotal += pointsList[j][0] * pointsList[j][1]
+		yTotal += pointsList[j][1]
+		oneTotal += 1
+		xValues = append(xValues, pointsList[j][0])
+		yValues = append(yValues, pointsList[j][1])
+	}
+	firstMatrix := [2][2]float64{{xSquaredTotal, xTotal}, {xTotal, oneTotal}}
+	equalVector := [2]float64{xyTotal, yTotal}
+	var inverseFirstMatrix [2][2]float64 = inverseMatrix2d(firstMatrix)
+	var mbTerms [2]float64
+	mbTerms = matrixTransform2d(inverseFirstMatrix, equalVector)
+	return mbTerms
+}
+
 func main() {
-	pointsList := [][]float64{{0, 0}, {1, 0.6}, {2, 3.5}, {4, 4}, {5, 7}}
+	pointsList := [][2]float64{{0, 0}, {1, 0.6}, {2, 3.5}, {4, 4}, {5, 7}}
 	var xSquaredTotal, xTotal, xyTotal, yTotal, oneTotal float64
 	var j int
 	var xValues, yValues []float64
