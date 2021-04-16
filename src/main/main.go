ac8600f64c64daaf1e62e32b8cc8c22a13215864
lneukirchner <lneukirchner123@gmail.com>
2021-04-16 10:31:52 -0400

    fixing problems

--- src/main/main.go
+++ src/main/main.go
@@ -4,13 +4,26 @@ import (
 	"fmt"
 )
 
-func determinant(matrix [][]float64) float64 {
-	var determinant float64
-	if len(matrix) > 2 {
+func determinant(matrix [][]float64) float64 { //Only works for 2x2 right now
+	size := [2]int{len(matrix), len(matrix[0])}
+	if size[0] != size[1] {
+		return 0
+	} else if size[0] > 2 {
+		var det float64
+		var j int
+		for j = 0; j < size[0]; j++ {
+			if j%2 == 0 {
 
+			} else if j%2 == 1 {
+
+			}
+		}
+	} else if size[0] == 2 {
+		return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
 	} else {
-
+		return matrix[0][0]
 	}
+
 }
 
 func matrixTransform2d(matrix [2][2]float64, vector [2]float64) [2]float64 {
@@ -18,10 +31,6 @@ func matrixTransform2d(matrix [2][2]float64, vector [2]float64) [2]float64 {
 	result[0] = (vector[0] * matrix[0][0]) + (vector[1] * matrix[1][0]) //x component
 	result[1] = (vector[0] * matrix[0][1]) + (vector[1] * matrix[1][1]) //y component
 	return result
-}
-
-func determinant2d(matrix [2][2]float64) float64 {
-	return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0] //ad-bc formula
 }
 
 func constantTimesMatrix2d(constant float64, matrix [2][2]float64) [2][2]float64 {
@@ -32,8 +41,8 @@ func constantTimesMatrix2d(constant float64, matrix [2][2]float64) [2][2]float64
 	return matrix
 }
 
-func inverseMatrix2d(matrix [2][2]float64) [2][2]float64 {
-	var inverseDeterminant float64 = 1 / determinant2d(matrix)
+func inverseMatrix2d(matrix [][]float64) [2][2]float64 {
+	var inverseDeterminant float64 = 1 / determinant(matrix)
 	var newMatrix [2][2]float64
 	newMatrix[0][0] = matrix[1][1]
 	newMatrix[0][1] = -matrix[0][1]
@@ -46,17 +55,14 @@ func inverseMatrix2d(matrix [2][2]float64) [2][2]float64 {
 func linearRegression2d(pointsList [][2]float64) [2]float64 {
 	var xSquaredTotal, xTotal, xyTotal, yTotal, oneTotal float64
 	var j int
-	var xValues, yValues []float64
 	for j = 0; j < len(pointsList); j++ {
 		xSquaredTotal += pointsList[j][0] * pointsList[j][0]
 		xTotal += pointsList[j][0]
 		xyTotal += pointsList[j][0] * pointsList[j][1]
 		yTotal += pointsList[j][1]
 		oneTotal += 1
-		xValues = append(xValues, pointsList[j][0])
-		yValues = append(yValues, pointsList[j][1])
 	}
-	firstMatrix := [2][2]float64{{xSquaredTotal, xTotal}, {xTotal, oneTotal}}
+	firstMatrix := [][]float64{{xSquaredTotal, xTotal}, {xTotal, oneTotal}}
 	equalVector := [2]float64{xyTotal, yTotal}
 	var inverseFirstMatrix [2][2]float64 = inverseMatrix2d(firstMatrix)
 	var mbTerms [2]float64
@@ -65,23 +71,7 @@ func linearRegression2d(pointsList [][2]float64) [2]float64 {
 }
 
 func main() {
-	pointsList := [][2]float64{{0, 0}, {1, 0.6}, {2, 3.5}, {4, 4}, {5, 7}}
-	var xSquaredTotal, xTotal, xyTotal, yTotal, oneTotal float64
-	var j int
-	var xValues, yValues []float64
-	for j = 0; j < len(pointsList); j++ {
-		xSquaredTotal += pointsList[j][0] * pointsList[j][0]
-		xTotal += pointsList[j][0]
-		xyTotal += pointsList[j][0] * pointsList[j][1]
-		yTotal += pointsList[j][1]
-		oneTotal += 1
-		xValues = append(xValues, pointsList[j][0])
-		yValues = append(yValues, pointsList[j][1])
-	}
-	firstMatrix := [2][2]float64{{xSquaredTotal, xTotal}, {xTotal, oneTotal}}
-	equalVector := [2]float64{xyTotal, yTotal}
-	var inverseFirstMatrix [2][2]float64 = inverseMatrix2d(firstMatrix)
-	var mbTerms [2]float64
-	mbTerms = matrixTransform2d(inverseFirstMatrix, equalVector)
-	fmt.Print("y = ", mbTerms[0], "x + (", mbTerms[1], ")")
+	myMatrix := [][]float64{{1, 2}, {2, 1}}
+	myMatrixDeterminant := determinant(myMatrix)
+	fmt.Println(myMatrixDeterminant)
 }
